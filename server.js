7543743932f1aad0e61346e63f1146e0c2ba2f86
anuradha.p.agrawal <anuradha.p.agrawal@accenture.com>
2020-10-28 13:18:39 +0530

    file modified

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + './dist/mySpaceXLaunchApp'));
 
-app.get('/*', function(req,res) {  
-    res.sendFile('./dist/mySpaceXLaunchApp/index.html');   
-});  
+// app.get('/*', function(req,res) {  
+//     res.sendFile('./dist/mySpaceXLaunchApp/index.html');   
+// });  
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
